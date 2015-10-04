c7c7781a13afedd0f125002af13e7151fc84029a
Vladimir Rutsky <vladimir@rutsky.org>
2015-10-05 01:05:42 +0300

    move metainformation test to unit/ submodule

---
+++ tests/unit/test___about__.py
@@ -0,0 +1,29 @@
+# Copyright 2015 Vladimir Rutsky <vladimir@rutsky.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test aiohttp_cors package metainformation.
+"""
+
+import unittest
+from pkg_resources import parse_version
+
+import aiohttp_cors
+
+
+class TestMetaInformation(unittest.TestCase):
+    """Test package metainformation"""
+    # pylint: disable=no-self-use
+    def test_version(self):
+        """Test package version string"""
+        parse_version(aiohttp_cors.__version__)

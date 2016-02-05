c05f131ac9db5d9e79c0e93e5946e9b5b6e58a48
Vladimir Rutsky <vladimir@rutsky.org>
2016-02-05 03:59:34 +0300

    fix aiohttp_cors to work with aiohttp==0.21
    This commit adds workaround for missing "match()" method, see
    <https://github.com/KeepSafe/aiohttp/issues/766>.
    
    Adds support in RouterAdapter for Resource-based routes.
    
    However configuring CORS for Resources is not yet supported.
    
    This change will make pre-0.21 Route-based user code to work with
    aiohttp 0.21, if user doesn't used name-based lookup of routes
    (which is one of backward incompatible changes in 0.21).

--- aiohttp_cors/urldispatcher_router_adapter.py
+++ aiohttp_cors/urldispatcher_router_adapter.py
@@ -16,7 +16,9 @@
 """
 
 import re
+from pkg_resources import parse_version
 
+import aiohttp
 from aiohttp import web
 
 from .abc import AbstractRouterAdapter
@@ -24,7 +26,11 @@ from .abc import AbstractRouterAdapter
 __all__ = ("UrlDispatcherRouterAdapter",)
 
 
-class UrlDispatcherRouterAdapter(AbstractRouterAdapter):
+_AIOHTTP_0_21 = parse_version(aiohttp.__version__) >= parse_version('0.21.0')
+
+
+# Router adapter for aiohttp < 0.21.0
+class _UrlDispatcherRouterAdapter_v20(AbstractRouterAdapter):
     """Router adapter for aiohttp.web.UrlDispatcher"""
     def __init__(self, router: web.UrlDispatcher):
         self._router = router
@@ -56,3 +62,53 @@ class UrlDispatcherRouterAdapter(AbstractRouterAdapter):
             raise RuntimeError("Unhandled route type", route)
 
         return new_route
+
+
+# Router adapter for aiohttp >= 0.21.0
+class _UrlDispatcherRouterAdapter_v21(AbstractRouterAdapter):
+    """Router adapter for aiohttp.web.UrlDispatcher"""
+    def __init__(self, router: web.UrlDispatcher):
+        self._router = router
+
+    def route_methods(self, route: web.AbstractRoute):
+        """Returns list of HTTP methods that route handles"""
+        return [route.method]
+
+    def add_options_method_handler(self, route: web.AbstractRoute, handler):
+        method = "OPTIONS"
+
+        if isinstance(route, web.ResourceRoute):
+            # Route added through Resource API.
+            new_route = route.resource.add_route(method, handler)
+
+        elif isinstance(route, web.StaticRoute):
+            # Route added through add_static() - not handled as ResourceRoute
+            # in aiohttp 0.21.0.
+
+            # TODO: Use custom matches that uses `str.startswith()` if
+            # regexp performance is not enough.
+            pattern = re.compile("^" + re.escape(route._prefix))
+            new_route = web.DynamicRoute(
+                method, handler, None, pattern, "")
+            self._router.register_route(new_route)
+
+        elif isinstance(route, web.PlainRoute):
+            # May occur only if user manually creates PlainRoute.
+            new_route = self._router.add_route(method, route._path, handler)
+
+        elif isinstance(route, web.DynamicRoute):
+            # May occur only if user manually creates DynamicRoute.
+            new_route = web.DynamicRoute(
+                method, handler, None, route._pattern, route._formatter)
+            self._router.register_route(new_route)
+
+        else:
+            raise RuntimeError("Unhandled route type", route)
+
+        return new_route
+
+
+if _AIOHTTP_0_21:
+    UrlDispatcherRouterAdapter = _UrlDispatcherRouterAdapter_v21
+else:
+    UrlDispatcherRouterAdapter = _UrlDispatcherRouterAdapter_v20

6a8e98ee9634cf7cd3fbbb6abd486497e77fea1e
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-01-03 13:02:56 +0200

    Update years

--- aiohttp_cors/__about__.py
+++ aiohttp_cors/__about__.py
@@ -20,9 +20,9 @@ This module must be stand-alone executable.
 
 __title__ = "aiohttp-cors"
 __version__ = "0.6.0"
-__author__ = "Vladimir Rutsky"
+__author__ = "Vladimir Rutsky and aio-libs team"
 __email__ = "vladimir@rutsky.org"
 __summary__ = "CORS support for aiohttp"
 __uri__ = "https://github.com/aio-libs/aiohttp-cors"
 __license__ = "Apache License, Version 2.0"
-__copyright__ = "2015, 2016, 2017 {}".format(__author__)
+__copyright__ = "2015-2018 {}".format(__author__)

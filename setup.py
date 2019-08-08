76bba3858ea26c3b85d7397b083e17991c52de3a
Josh Holland <jh36@sanger.ac.uk>
2019-08-08 14:21:42 +0100

    Mark the package as not zip-safe
    zip_safe=False is required for mypy to find the py.typed marker:
    https://mypy.readthedocs.io/en/latest/installed_packages.html#making-pep-561-compatible-packages

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
     )),
     packages=["aiohttp_cors"],
     include_package_data=True,
+    zip_safe=False,
     setup_requires=[
         # Environment markers were implemented and stabilized in setuptools
         # v20.8.1 (see <http://stackoverflow.com/a/32643122/391865>).
